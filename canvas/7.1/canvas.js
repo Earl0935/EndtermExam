dd73c56c27deaee7e0e9aed1c03c9429d5d98747
Earl0935 <60341137+Earl0935@users.noreply.github.com>
2020-12-22 22:21:30 +0800

    Add files via upload

---
+++ canvas/7.1/canvas.js
@@ -0,0 +1,351 @@
+var canvas = document.getElementById("canvas1");
+var ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.moveTo(15,15);
+ctx.lineTo(186,15);
+ctx.lineTo(186,188);
+ctx.lineTo(15,188);
+ctx.lineTo(15,15);
+ctx.closePath();
+ctx.fillStyle = "#A59898";
+ctx.fill();	
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(46, 46);
+ctx.lineTo(156, 46);
+ctx.lineTo(156, 156);
+ctx.lineTo(46, 156);
+ctx.lineTo(46, 46);
+ctx.closePath();
+ctx.fillStyle = "#3C3939";
+ctx.fill();	
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(78, 78);
+ctx.lineTo(125, 78);
+ctx.lineTo(125, 125);
+ctx.lineTo(78, 125);
+ctx.lineTo(78, 78);
+ctx.closePath();
+ctx.fillStyle = "#F3EEEE";
+ctx.fill();	
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(78, 78);
+ctx.lineTo(125, 78);
+ctx.lineTo(125, 125);
+ctx.lineTo(78, 125);
+ctx.lineTo(78, 78);
+ctx.closePath();
+ctx.fillStyle = "#F3EEEE";
+ctx.fill();	
+ctx.stroke();
+
+
+ctx.beginPath();
+ctx.moveTo(128, 30);
+ctx.lineTo(155, 31);
+ctx.lineTo(158, 27);
+ctx.lineTo(156, 24);
+ctx.lineTo(127, 24);
+ctx.lineTo(124, 27);
+ctx.lineTo(128, 30);
+ctx.closePath();
+ctx.fillStyle = "#F3EEEE";
+ctx.fill();	
+ctx.stroke();
+
+
+ctx.beginPath();
+ctx.moveTo(165, 17);
+ctx.lineTo(181, 17);
+ctx.lineTo(181, 35);
+ctx.lineTo(165, 35);
+ctx.lineTo(165, 17);
+ctx.closePath();
+ctx.fillStyle = "#F3EEEE";
+ctx.fill();	
+ctx.stroke();
+
+
+
+
+
+var canvas = document.getElementById("canvas2");
+var ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.moveTo(31, 171);
+ctx.lineTo(62, 171);
+ctx.lineTo(93, 63);
+ctx.lineTo(125, 171);
+ctx.lineTo(156, 171);
+ctx.lineTo(140, 31);
+ctx.lineTo(46, 31);
+ctx.lineTo(31, 171);
+ctx.closePath();
+ctx.fillStyle = "#1942E9";
+ctx.fill();
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(96, 59);
+ctx.lineTo(96, 31);
+ctx.lineTo(93, 31);
+ctx.lineTo(93, 62);
+ctx.lineTo(96, 59);
+ctx.closePath();
+ctx.fillStyle = "#0C237E";
+ctx.fill();
+ctx.stroke();
+
+
+var canvas = document.getElementById("canvas3");
+var ctx = canvas.getContext("2d");
+
+
+ctx.beginPath();
+ctx.moveTo(50, 31);
+ctx.lineTo(93, 109);
+ctx.lineTo(93, 140);
+ctx.lineTo(31, 31);
+ctx.lineTo(50, 31);
+ctx.closePath();
+ctx.fillStyle = "#FF0000";
+ctx.fill();
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(89, 75);
+ctx.lineTo(110, 31);
+ctx.lineTo(68 ,31);
+ctx.lineTo(89, 75);
+ctx.closePath();
+ctx.fillStyle = "#FF0000";
+ctx.fill();
+ctx.stroke();
+
+var canvas = document.getElementById("canvas4");
+var ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.moveTo(89, 14);
+ctx.lineTo(173, 85);
+ctx.lineTo(104, 166);
+ctx.lineTo(20, 95);
+ctx.lineTo(89, 14);
+ctx.closePath();
+ctx.fillStyle = "#B9B5B5";
+ctx.fill();
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(118, 39);
+ctx.lineTo(123, 43);
+ctx.lineTo(91, 81);
+ctx.lineTo(86, 76);
+ctx.lineTo(118, 39);
+ctx.closePath();
+ctx.fillStyle = "#fff";
+ctx.fill();
+
+
+
+ctx.beginPath();
+ctx.moveTo(187, 94);
+ctx.lineTo(118, 178);
+ctx.lineTo(106, 169);
+ctx.lineTo(175, 84);
+ctx.lineTo(187, 94);
+ctx.closePath();
+ctx.fillStyle = "#0962F4";
+ctx.fill();
+ctx.stroke();
+
+
+ctx.beginPath();
+ctx.moveTo(19, 96);
+ctx.lineTo(101, 168);
+ctx.lineTo(91, 180);
+ctx.lineTo(9, 108);
+ctx.lineTo(19, 96);
+ctx.closePath();
+ctx.fillStyle = "#0962F4";
+ctx.fill();
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(104, 170);
+ctx.lineTo(116, 180);
+ctx.lineTo(106, 192);
+ctx.lineTo(94 ,182);
+ctx.lineTo(104, 170);
+ctx.closePath();
+ctx.fillStyle = "#0962F4";
+ctx.fill();
+ctx.stroke();
+
+
+
+var canvas = document.getElementById("canvas5");
+var ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.moveTo(125, 31);
+ctx.lineTo(125, 46);
+ctx.lineTo(171, 46);
+ctx.lineTo(171, 93);
+ctx.lineTo(187, 93);
+ctx.lineTo(187, 31);
+ctx.lineTo(125, 31);
+ctx.closePath();
+ctx.fillStyle = "#0C237E";
+ctx.fill();
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(93, 187);
+ctx.lineTo(93, 171);
+ctx.lineTo(46, 171);
+ctx.lineTo(46, 125);
+ctx.lineTo(31, 125);
+ctx.lineTo(31, 187);
+ctx.lineTo(93, 187);
+ctx.closePath();
+ctx.fillStyle = "#0C237E";
+ctx.fill();
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(187, 125);
+ctx.lineTo(171, 125);
+ctx.lineTo(171, 171);
+ctx.lineTo(125, 171);
+ctx.lineTo(125, 187);
+ctx.lineTo(187, 187);
+ctx.lineTo(187, 125);
+ctx.closePath();
+ctx.fillStyle = "#0C237E";
+ctx.fill();
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(31, 93);
+ctx.lineTo(46, 93);
+ctx.lineTo(46, 46);
+ctx.lineTo(93, 46);
+ctx.lineTo(93, 31);
+ctx.lineTo(31, 31);
+ctx.lineTo(31, 93);
+ctx.closePath();
+ctx.fillStyle = "#0C237E";
+ctx.fill();
+ctx.stroke();
+
+var canvas = document.getElementById("canvas6");
+var ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.moveTo(4, 114);
+ctx.lineTo(122, 191);
+ctx.lineTo(190, 86);
+ctx.lineTo(177, 78);
+ctx.lineTo(117, 169);
+ctx.lineTo(26 ,110);
+ctx.lineTo(85, 18);
+ctx.lineTo(72, 10);
+ctx.lineTo(4 ,114);
+ctx.closePath();
+ctx.fillStyle = "#0C237E";
+ctx.fill();
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(138, 52);
+ctx.lineTo(95 ,118);
+ctx.lineTo(82 ,109);
+ctx.lineTo(125, 44);
+ctx.lineTo(138, 52);
+ctx.closePath();
+ctx.fillStyle = "#0C237E";
+ctx.fill();
+ctx.stroke();
+
+
+var canvas = document.getElementById("canvas7");
+var ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.moveTo(139, 156);
+ctx.lineTo(101, 198);
+ctx.lineTo(98, 198);
+ctx.lineTo(60, 156);
+ctx.lineTo(76, 33);
+ctx.lineTo(123, 33);
+ctx.lineTo(139, 156);
+ctx.closePath();
+ctx.fillStyle = "#0C237E";
+ctx.fill();
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(76, 33);
+ctx.lineTo(123, 33);
+ctx.lineTo(139, 1);
+ctx.lineTo(60, 1);
+ctx.lineTo(76, 33);
+ctx.closePath();
+ctx.fillStyle = "#0C237E";
+ctx.fill();
+ctx.stroke();
+
+var canvas = document.getElementById("canvas8");
+var ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.moveTo(31, 31);
+ctx.lineTo(93, 31);
+ctx.lineTo(93, 93);
+ctx.lineTo(31, 93);
+ctx.lineTo(31, 31);
+ctx.closePath();
+ctx.fillStyle = "#F40909";
+ctx.fill();
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(96, 31);
+ctx.lineTo(159, 31);
+ctx.lineTo(159, 93);
+ctx.lineTo(96, 93);
+ctx.lineTo(96, 31);
+ctx.closePath();
+ctx.fillStyle = "#0EC80E";
+ctx.fill();
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(31, 96);
+ctx.lineTo(93, 96);
+ctx.lineTo(93, 159);
+ctx.lineTo(31, 159);
+ctx.lineTo(31, 96);
+ctx.closePath();
+ctx.fillStyle = "#0E90C8";
+ctx.fill();
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(96, 96);
+ctx.lineTo(159, 96);
+ctx.lineTo(159, 159);
+ctx.lineTo(96, 159);
+ctx.lineTo(96, 96);
+ctx.closePath();
+ctx.fillStyle = "#FFFF00";
+ctx.fill();
+ctx.stroke();
---
+++ css/Exam/index.php
@@ -0,0 +1,76 @@
+<!DOCtype html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="style.css" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<title> Home </title>
+</head>
+<body>
+
+	<div id="Main">
+		<div class="header">
+		
+			<a href><div id="search">
+			<span id="search1"></span>
+			<span id="search2"></span>
+			</div></a>
+			
+			<div class="title">
+			
+			
+			</div>
+			
+			<div class="logoboard">
+				<div id="logo">
+				<span id="logo1"></span>
+				<span id="logo2"></span>
+				
+				</div>
+			</div>
+			
+			
+	
+		</div>
+		<div class="content">
+			<div id="sidemenu">
+				<ul>
+					<li><a href>HOME</a></li>
+					<li><a href>ABOUT ME</a></li>
+					<li><a href>CONTACTS</a></li>
+				</ul>
+			</div>
+			<div id="main">
+				<h1>Wanna learn more about me?</h1>
+				<h1>This Site is all about me</h1>
+				<h1>Enjoy</h1>
+			</div>
+		</div>
+		
+		
+		<footer> 
+			<ul class="circles">
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+		</ul>
+			<i class="fa fa-facebook fa-3x" aria-hidden="true"></i>
+			<i class="fa fa-instagram fa-3x" aria-hidden="true"></i>
+			<i class="fa fa-twitter fa-3x" aria-hidden="true"></i>
+		</footer>
+		
+		
+	</div>
+
+
+
+
+</body>
+</html>
+
